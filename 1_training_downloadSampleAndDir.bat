16a62a45286c1e262c17ea0264132de7d19d2548
kevinhsu <tkdstkdstkds@hotmail.com>
2023-06-11 23:37:43 +0800

    feat: complete 1_training_downloadSampleAndDir.bat feat: add git ignore feat: add install_requirements.txt doc for future installing.

---
+++ 1_training_downloadSampleAndDir.bat
@@ -0,0 +1,25 @@
+@echo off
+
+set sampleAudioName=sampled_audio4ft_v2.zip
+set sampleAudioURL=https://huggingface.co/datasets/Plachta/sampled_audio4ft/resolve/main/sampled_audio4ft_v2.zip
+
+if exist "%sampleAudioName%" (
+    echo %sampleAudioName% already exists. Skipping download.
+) else (
+    curl -L -o "%sampleAudioName%" "%sampleAudioURL%"
+)
+
+set sampleAudioFolder=%sampleAudioName:_v2.zip=%
+if exist "%sampleAudioFolder%" (
+    echo %sampleAudioFolder% already exists. Skipping tar.
+) else (
+    tar -xf %sampleAudioName% %sampleAudioFolder%
+)
+
+mkdir video_data
+mkdir raw_audio
+mkdir denoised_audio
+mkdir custom_character_voice
+mkdir segmented_character_voice
+
+pause
